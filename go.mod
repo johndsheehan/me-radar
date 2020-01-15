d8553363d642e15a2dfa765aa25e456590ce7ee1
John D Sheehan <john.d.sheehan@gmail.com>
2020-01-15 22:29:42 +0000

    add optional https
    Signed-off-by: John D Sheehan <john.d.sheehan@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/johndsheehan/met-eireann-archive
 
 go 1.12
 
-require golang.org/x/image v0.0.0-20190424155947-59b11bec70c7
+require golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
--- serve.go
+++ serve.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func serveRadar(rdr *Radar) func(http.ResponseWriter, *http.Request) {
@@ -15,10 +17,55 @@ func serveRadar(rdr *Radar) func(http.ResponseWriter, *http.Request) {
 		w.Write(g)
 	}
 }
-func serve(r *Radar, colonPort string) {
+
+func redirectToHTTPS(colonPort, colonTLSPort string) {
+	go func(colonPort, colonTLSPort string) {
+		redirect := func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("http request from: %s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+			host := r.Host
+			if strings.HasSuffix(r.Host, colonPort) {
+				host = strings.TrimSuffix(host, colonPort)
+			}
+
+			url := fmt.Sprintf("https://%s%s/%s", host, colonTLSPort, r.RequestURI)
+			log.Printf("redirect to: %s", url)
+
+			http.Redirect(w, r, url, http.StatusMovedPermanently)
+		}
+
+		err := http.ListenAndServe(colonPort, http.HandlerFunc(redirect))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(colonPort, colonTLSPort)
+}
+
+type ServerConfig struct {
+	colonPort    string
+	colonTLSPort string
+	fullchain    string
+	privateKey   string
+	useTLS       bool
+}
+
+func serve(r *Radar, cfg ServerConfig) {
 	http.HandleFunc("/", serveRadar(r))
-	err := http.ListenAndServe(colonPort, nil)
-	if err != nil {
-		log.Print(err)
+
+	if cfg.useTLS {
+		go redirectToHTTPS(cfg.colonPort, cfg.colonTLSPort)
+
+		log.Printf("serving https on %s\n", cfg.colonTLSPort)
+		err := http.ListenAndServeTLS(cfg.colonTLSPort, cfg.fullchain, cfg.privateKey, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Printf("serving http on %s\n", cfg.colonPort)
+
+		err := http.ListenAndServe(cfg.colonPort, nil)
+		if err != nil {
+			log.Print(err)
+		}
 	}
 }
